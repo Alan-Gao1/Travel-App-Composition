06b1ff7ad05ccf883dd045957da5f6acc3d37a1c
Alan Gao <agao1@bayviewglen.ca>
2023-03-01 19:29:05 -0500

    HAHA IM DONE :D

---
+++ vue-school-travel-app/src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Not Found</h1>
+        <p>Couldn't find that page :( try going to
+            <router-link to="/">home</router-link>
+        </p>
+    </div>
+</template>
